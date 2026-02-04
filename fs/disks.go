df0ef8e21890f7af8bd869dc3ce059d9daa93a12
AYn0nyme <aynonyme@proton.me>
2026-02-04 14:45:05 -0400

    feat(fs/disks.go): add GetPartitionType

--- fs/disks.go
+++ fs/disks.go
@@ -1,10 +1,27 @@
 package fs
 
 import (
+	"encoding/json"
+	"errors"
 	"os"
 	"os/exec"
 	"path"
+	"strings"
 )
+
+type PartitionType int
+
+const (
+	PartitionTypeEFI PartitionType = iota
+	PartitionTypeLinuxFileSystem
+	PartitionTypeLinuxSwap
+)
+
+type ListBlockPayload struct {
+	BlockDevices []struct{
+		PartitionTypeName string `json:"parttypename"`
+	} `json:"blockdevices"`
+}
 
 func GetDisks() ([]string, error) {
 	var disks []string
@@ -36,9 +53,35 @@ func EditDisk(disk string) error {
 	}
 	cmd = exec.Command("clear")
 
-	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
 	cmd.Run()
 	return nil
+}
+
+func DetectDiskLayout() string {
+	return ""
+}
+
+func GetPartitionType(part string) (PartitionType, error) {
+	ret, err := exec.Command("lsblk", "-JO", part).Output()
+	if err != nil {
+		return -1, err
+	}
+
+	var payload ListBlockPayload
+
+	err = json.Unmarshal(ret, &payload); if err != nil {
+		return -1, err
+	}
+
+	switch(strings.ToLower(payload.BlockDevices[0].PartitionTypeName)) {
+	case "efi system":
+		return PartitionTypeEFI, nil
+	case "linux filesystem":
+		return PartitionTypeLinuxFileSystem, nil
+	case "linux swap":
+		return PartitionTypeLinuxSwap, nil
+	default:
+		return -2, errors.New("unsupported partition type")
+	}
 }
